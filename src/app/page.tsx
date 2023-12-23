cdd2c8c675be82a73e2af55874cb3d1e1c354351
GUAN-MING <guanmingchiu@gmail.com>
2023-12-23 21:59:39 +0800

    refactor: move Space component to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
-import Space from "@/components/space";
-
 export default function Home() {
   return (
     <main className="flex h-screen flex-col-reverse items-center justify-between">
-      <div className="absolute z-10 h-full w-full">
-        <Space></Space>
-      </div>
-
-      <div className="max-lg: z-20 flex w-full items-end justify-between px-16 py-8 max-sm:px-8 max-sm:py-4">
+      <div className="z-20 flex w-full items-end justify-between px-16 py-8 max-sm:px-8 max-sm:py-4">
         <div className="flex-col space-y-2">
-          <p className="z-20 bg-transparent text-5xl font-bold max-sm:text-lg">
+          <p className="z-20 bg-transparent text-5xl font-bold max-sm:text-2xl">
             GUAN MING
           </p>
-          <p className="z-20 bg-transparent text-5xl font-bold max-sm:text-lg">
+          <p className="z-20 bg-transparent text-5xl font-bold max-sm:text-2xl">
             CHIU
           </p>
         </div>
